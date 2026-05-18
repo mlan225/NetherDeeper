74b9dc4d03f574397c87f9f7ef42bd686b2454a8
mlan225 <17088502+mlan225@users.noreply.github.com>
2026-05-18 01:22:59 -0600

    [In-progress Milestone 6] Create player damage animation and invincibility frames
    Will need to finish implementing the health and health bar components

---
+++ components/player/player_health_component.gd
@@ -0,0 +1,4 @@
+extends Node
+
+#DEVNOTE: Going to create a health component to handle the logic and a 
+#healthbar component to handle displaying health values
--- components/player/player_hurtbox_component.gd
+++ components/player/player_hurtbox_component.gd
@@ -4,13 +4,35 @@ extends Node
 @export_group("Nodes")
 @export var player: CharacterBody2D
 @export var player_hurt_area: Area2D
+#DEVNOTE: temporary player character model until a sprite is implimented
+@export var color_rect = ColorRect
+
+#DEVNOTE: Temporary color rect for player. Will replace with sprite
+var _color_rect_original_color: Color
+var _is_hurt: bool = false
 
 func _ready() -> void:
+	_color_rect_original_color = color_rect.color
+	
 	if player_hurt_area:
 		player_hurt_area.area_entered.connect(_on_hurt_area_entered)
 	else:
 		push_warning("HurtboxComponent: HurtboxComponent: No Area2D assigned to 'player_hurt_area'")
 		
 func _on_hurt_area_entered(area: Area2D):
-	if area.is_in_group("enemy/hitbox"):
-		print("player has been hit! [Player weapon hurtbox is temporarily disabled for testing]")
+	if not _is_hurt:
+		if area.is_in_group("enemy/hitbox"):
+			#DEVNOTE: will be adding arguments for amount of damage to be taken
+			take_damage()
+
+#DEVNOTE: Damage to take will be set later for different sources
+func take_damage(damage_to_take:int = 0):
+	print("player took damage")
+	#DEVNOTE: Temporary for now until it is replaced with sprite
+	color_rect.color = Color.RED
+	_is_hurt = true
+	player_hurt_area.set_deferred("monitoring", false)
+	await get_tree().create_timer(0.5).timeout
+	_is_hurt = false
+	player_hurt_area.set_deferred("monitoring", true)
+	color_rect.color = _color_rect_original_color
